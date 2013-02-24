0a79dcbcae9543e5df8566076762218280d21a82
Stephane Liu <sliu@sjliu.com>
2013-02-24 13:45:57 -1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HawaiianAir::Application.config.secret_token = 'd65b3056b6a4af091235aec3822ebc2c197a6632871a95cbf3c71188809e9605f56ffa24f715d4f6764691dc044d2ece9cfcc5ee9556f009eca62b347d792a2e'
