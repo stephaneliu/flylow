9de42d2026c9c4f3920cc5dd8b503929fa8129b4
Stephane Liu <sliu@sjliu.com>
2013-03-01 15:39:15 -1000

    Change parsing method of page

--- app/models/scrap.rb
+++ app/models/scrap.rb
@@ -1,63 +1,50 @@
 class Scrap
 
-  attr_accessor :origin, :destination, :travelers
+  attr_accessor :origin, :destination, :travelers, :departure_date
 
   # origin / destintaion - airport.code
   def initialize(origin, destination, options={})
-    @agent        = create_agent
-    @origin       = origin
-    @destination  = destination
-
-    @travelers    = options[:travelers] || 2
-    @start_month  = options[:start_month] || Time.now.month
-    # @round_trip   = options[:round_trip] || true
-
-    
-
-    # @page   = @agent.get start_url
-    # form    = get_form(@page)
-
-    # fill_form(form)
-    # page = agent.submit(form)
-
+    options         = {} unless options.is_a? Hash # ignore unless options
+    @agent          = create_secure_agent
+    @origin         = origin
+    @destination    = destination
+    @travelers      = options[:travelers] || 2
+    #@departure_date = format_departure(options[:departure_date] || 1.day.from_now.localtime)
+    @departure_date = format_departure 1.day.from_now.localtime
   end
 
   def get_fares_by_day(outbound=true)
-    day_with_fare = {}
     page          = get_page(outbound)
 
-    # non-cheap fares
-    page.css('td.CalendarDayDefault').each_with_object(day_with_fare) do |day_fare, all| 
-      day, fare = day_fare.content.split("$")
+    page.css('td.CalendarDayDefault').each_with_object({}) do |day_fare, all| 
+      day   = day_fare.css('.Text').first.content
+      fare  = day_fare.css('.Fare').first.content
+
       puts "day: #{day} / fare: #{fare}"
-      all[day]  = fare
+
+      all[day] = fare
+      # day, fare = day_fare.content.split("$")
+      # all[day]  = fare
     end
-    day_with_fare
   end
 
   def temp_url
     "https://fly.hawaiianairlines.com/Calendar/Calendar.aspx?orig=PDX&dest=HNL&traveler=1&isDM=false&isRoundTrip=true&depDate=5/9/2013&owORob=false&isEAward=false"
   end
 
-  def working_example
-    agent = Mechanize.new
-    agent.ssl_version = 'SSLv3'
-    file = agent.get "https://fly.hawaiianairlines.com/Calendar/Calendar.aspx?orig=PDX&dest=HNL&traveler=1&isDM=false&isRoundTrip=true&depDate=5/9/2013&owORob=false&isEAward=false"
-    page = Nokogiri::HTML.parse "<html>#{file.content}</html>"
-    #page.css('td.CalendarDayDefault > div.Fare').each {|fare| fare.content}
-  end
-
   private 
 
-  def create_agent
+  def format_departure(date_or_time)
+    unless [ActiveSupport::TimeWithZone, Time, Date].include? date_or_time.class
+      raise ArgumentError, "Expected departure_date to be a Date or Time"
+    end
+    date_or_time.strftime('%m/%d/%y')
+  end
+
+  def create_secure_agent
     agent              = Mechanize.new
     agent.ssl_version  = 'SSLv3'
     agent
-  end
-
-  def departure_date
-    tomorrow = 1.day.from_now.localtime
-    "#{tomorrow.month}/#{tomorrow.day}/#{tomorrow.year.to_s[-2..-1]}"
   end
 
   def get_page(outbound=true)
@@ -66,7 +53,7 @@ class Scrap
   end
 
   def calendar_url(outbound=true)
-    "https://fly.hawaiianairlines.com/Calendar/Calendar.aspx?orig=#{origin}&dest=#{destination}&traveler=#{travelers}&depDate=#{departure_date}&owORob=#{outbound}&isDM=false&isRoundTrip=true&isEAward=false".tap { |url| puts url}
+    "https://fly.hawaiianairlines.com/Calendar/Calendar.aspx?orig=#{origin}&dest=#{destination}&traveler=#{travelers}&depDate=#{@departure_date}&owORob=#{outbound}&isDM=false&isRoundTrip=true&isEAward=false".tap { |url| puts url}
   end
 
 end
