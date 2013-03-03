e039965d248948754260f3588740cde2e943a180
Stephane Liu <sliu@sjliu.com>
2013-03-02 23:39:32 -1000

    Remove sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,9 +28,8 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable, :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
