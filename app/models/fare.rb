c3e9abfe2571d1d68e10d852914e2d38413d3400
Stephane Liu <sliu@sjliu.com>
2013-03-02 15:43:40 -1000

    Annotate

--- app/models/fare.rb
+++ app/models/fare.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+# Schema version: 20130302222428
+#
+# Table name: fares
+#
+#  id             :integer          not null, primary key
+#  price          :decimal(8, 2)
+#  departure_date :date
+#  origin_id      :integer
+#  destination_id :integer
+#  comments       :text
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Fare < ActiveRecord::Base
   attr_accessible :price, :departure_date, :origin_id, :destination_id, :origin, :destination, :comments
 
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20130302222428
+#
+# Table name: cities
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  region       :string(255)
+#  airport_code :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  favorite     :boolean
+#
+
 require 'spec_helper'
 
 describe City do
