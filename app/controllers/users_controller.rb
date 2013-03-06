9da04d1ca1847e4b7c90dc19ea2e02c2131b6fe0
Stephane Liu <sliu@sjliu.com>
2013-03-05 15:31:14 -1000

    Fix authorization of user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 class UsersController < ApplicationController
 
-  authorize_resource
+  load_and_authorize_resource
 
   def index
-    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user], :as => :admin)
       redirect_to users_path, :notice => "User updated."
     else
@@ -20,9 +17,9 @@ class UsersController < ApplicationController
   end
     
   def destroy
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
+    # user = User.find(params[:id])
+    unless @user == current_user
+      @user.destroy
       redirect_to users_path, :notice => "User deleted."
     else
       redirect_to users_path, :notice => "Can't delete yourself."
