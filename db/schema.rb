711dc93e502033322a7c70d261256e32039776bb
Stephane Liu <sliu@sjliu.com>
2013-03-26 18:37:11 -1000

    Add low fare model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302222428) do
+ActiveRecord::Schema.define(:version => 20130327043541) do
 
   create_table "cities", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(:version => 20130302222428) do
     t.integer  "origin_id"
     t.integer  "destination_id"
     t.text     "comments"
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+  end
+
+  create_table "low_fares", :force => true do |t|
+    t.integer  "origin_id"
+    t.integer  "destination_id"
+    t.decimal  "price",          :precision => 8, :scale => 2
     t.datetime "created_at",                                   :null => false
     t.datetime "updated_at",                                   :null => false
   end
