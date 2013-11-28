96a63057698d0cc95978bd75c2004d87edec63b5
Stephane Liu <sliu@sjliu.com>
2013-11-27 17:19:43 -1000

    Remove ssl force in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ HawaiianAir::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -40,7 +40,7 @@ HawaiianAir::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store
+  # config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
