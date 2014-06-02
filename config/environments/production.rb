7c27e9b81c5894bc8eb49ff0ab468d5a3339b654
Stephane Liu <sliu@sjliu.com>
2014-06-01 22:21:59 -1000

    Yet more configuration changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ HawaiianAir::Application.configure do
   # Enable threaded mode
   config.eager_load = true
 
+  config.assets.js_compressor = :uglifier
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
