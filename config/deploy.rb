77d50f4dc8445681a0ac41328b7cb30bcb976c6b
Stephane Liu <sliu@sjliu.com>
2014-12-16 21:15:59 -1000

    Fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
+require 'etc'
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
+require 'mina/chruby'
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -8,43 +10,33 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-if RUBY_PLATFORM =~ /darwin/
-  id = "/Volumes/4T_home/sliu/.ssh/id_rsa"
-else
-  id = "/home/sliu/.ssh/id_rsa"
-end
 
 set :term_mode,    :pretty
 set :application,  "flylow"
 set :user,         'fly'
-set :ruby_version, "ruby-2.1.2"
+set :local_user,   Etc.getlogin
+set :ruby_version, "ruby-2.1.3"
 
 # Optional settings:
-set :identity_file,  id
+set :identity_file,  File.join(Dir.home(current_user), ".ssh", "id_rsa")
 set :ssh_options,    '-A' # key passthrough for git repo
 set :port,           "3838"
 
 set :domain,      "flylow.pixelatedpath.com"
-set :deploy_to,   "/home/#{user}/#{application}"
+set :deploy_to,   "/home/#{user}"
 set :app_path,    "#{deploy_to}/#{current_path}"
-set :repository,  "git@github.com:stephaneliu/hal_fare.git"
+set :repository,  "http://github.com/stephaneliu/hal_fare.git"
 set :branch,      "master"
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'config/application.yml', 'log', 'tmp']
 
+set :chruby_path, "/usr/local/share/chruby/chruby.sh"
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
+# This task is the environment that is loaded for most commands, such as `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  queue! %[source "/usr/local/share/chruby/chruby.sh"]
-  queue! %[chruby "#{ruby_version}"]
+  invoke :'chruby[ruby-2.1.3]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
@@ -57,9 +49,9 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/pids"]
-  queue! %[touch "#{deploy_to}/shared/tmp/pids/unicorn.pid"]
+  # queue! %[mkdir -p "#{deploy_to}/shared/tmp/pids"]
+  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/pids"]
+  # queue! %[touch "#{deploy_to}/shared/tmp/pids/unicorn.pid"]
 
   queue! %[mkdir -p "#{deploy_to}/shared/tmp/sockets"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/sockets"]
