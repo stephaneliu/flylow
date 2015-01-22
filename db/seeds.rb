c0f64ffd6f85bd5e6f62cbfa1f4573e969a605e9
Stephane Liu <sliu@sjliu.com>
2015-01-22 06:48:32 -1000

    Remove role creation from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@
 # See http://railsapps.github.com/rails-environment-variables.html
 puts 'ROLES'
 
-if Rails.env == "production"
-  %w(admin, user, VIP).each do |role|
-    Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  end
-else
-  YAML.load(ENV['ROLES']).each do |role|
-    Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-    puts 'role: ' << role
-  end
-  puts 'DEFAULT USERS'
-  user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-  puts 'user: ' << user.name
-  user.add_role :admin
-end
+# if Rails.env == "production"
+#   %w(admin, user, VIP).each do |role|
+#     Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#   end
+# else
+#   YAML.load(ENV['ROLES']).each do |role|
+#     Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#     puts 'role: ' << role
+#   end
+#   puts 'DEFAULT USERS'
+#   user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+#   puts 'user: ' << user.name
+#   user.add_role :admin
+# end
 
 { "Oahu" => ["HNL", "Domestic"], 
   "Hawaii Island - Hilo, HI" => ["ITO", "Domestic"],
