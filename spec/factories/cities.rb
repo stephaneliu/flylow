d1dbf28c79dcd5a8dbbeede822c8b256b01384c3
Stephane Liu <sliu@sjliu.com>
2015-04-01 22:46:15 -1000

    Factory HNL

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     sequence(:name) { |f| "city_name_#{f}" }
     region "Domestic"
     sequence(:airport_code) { ['PDX','SFO', 'SEA'].shuffle.first }
-    airport_code "PDX"
 
     trait :favorite do
       favorite true
@@ -20,5 +19,10 @@ FactoryGirl.define do
     factory :favorite_city, traits: [:favorite]
     factory :domestic_city, traits: [:domestic]
     factory :international_city, traits: [:international]
+  end 
+
+  factory :oahu, class: City do
+    airport_code 'HNL'
+    region 'Domestic'
   end
 end
