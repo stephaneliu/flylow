d45b5a19b403e40b81f8cec8c8889a7eae2d5c75
Stephane Liu <sliu@sjliu.com>
2015-04-29 21:34:33 -1000

    Refactor low_upcoming_fare_query to take departure parameter

---
+++ spec/query_objects/low_upcoming_fare_query_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe LowUpcomingFareQuery do
+  let(:origin)      { create(:city) }
+  let(:destination) { create(:city) }
+  let(:query)       { described_class.new(origin, destination) }
+
+  describe '#initialize' do
+    specify           { expect(query.low_upcoming_fares).to_not be_nil }
+
+    context 'when departure is false' do
+      it 'assigns origin to departure and departure to origin' do
+        expect(Fare).to receive(:where).with(origin_id: destination, destination_id: origin)
+        described_class.new(origin, destination, !:departure)
+      end
+    end
+  end
+
+  describe '#find_all' do
+    subject { query.find_all(Time.now, 2.days.from_now) }
+    specify { is_expected.to be_a ActiveRecord::Relation }
+  end
+end
