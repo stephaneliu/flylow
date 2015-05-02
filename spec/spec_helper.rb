e1845d3fc303da08aada5ede6e5756827fd57e96
Stephane Liu <sliu@sjliu.com>
2015-05-02 11:13:07 -1000

    Remove coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,9 +15,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'coveralls'
-
-Coveralls.wear!
+# require 'coveralls'
+# Coveralls.wear!
 
 RSpec.configure do |config|
   config.filter_run focus: true
