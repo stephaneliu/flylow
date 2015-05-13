8cf31c872ab21410f7432518ac5f52c0be35497b
Stephane Liu <sliu@sjliu.com>
2015-05-13 10:41:59 -1000

    Change figaro keys to string

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 #     Role.find_or_create_by_name({ :name => role }, :without_protection => true)
 #   end
 # else
-#   YAML.load(ENV['ROLES']).each do |role|
+#   YAML.load(ENV['ROLES'].split(",")).each do |role|
 #     Role.find_or_create_by_name({ :name => role }, :without_protection => true)
 #     puts 'role: ' << role
 #   end
