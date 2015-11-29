196083771d4979786af300463b6e3ae9d4cc5540
Stephane Liu <sliu@sjliu.com>
2015-11-28 23:44:32 -1000

    Configure should_matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,20 +17,7 @@
 #  updated_at             :datetime         not null
 #  name                   :string(255)
 #
-
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'email validity' do
-    it do
-      is_expected.to validate_presence_of :email
-      is_expected.to validate_uniqueness_of :email
-    end
-  end
-
-  describe 'password validity' do
-    it { is_expected.to validate_confirmation_of :password }
-    it { is_expected.to validate_presence_of :password }
-    it { is_expected.to validate_length_of(:password).is_at_least(8) }
-  end
 end
